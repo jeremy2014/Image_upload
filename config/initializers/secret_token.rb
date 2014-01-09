5e2a3bece1bd8886c7a32a3e0ab5c5e0e682a51e
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2014-01-09 12:44:43 +0800

    Uploading image using carrierwave gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carrierwave::Application.config.secret_key_base = 'cbb09ddf18318c024b284f55d4a6a379eeb586b22ac3cab0a634554985c220d9e025a5a62cd4a8ee316db47338dce3fdaa165b0b4fdddd5fe8861471cfd8445e'
